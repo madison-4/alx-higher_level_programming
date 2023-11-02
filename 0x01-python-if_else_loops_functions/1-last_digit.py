6c6a3bce32fb62e483783e33e2524b7bbee77f94
nox <samingosi97@gmail.com>
2023-11-01 19:24:33 -0700

    use range an ascii to print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 remainder = number % 10
-print(f'Last digit of {number:d} is', end=' ')
+if (number < 0):
+    number = -number
+    remainder = number % 10
+    remainder = -remainder
+    number = -number
+print(f'Last digit of {number: d} is', end=' ')
 if ((number % 10) > 5):
     print(f'{remainder:d} and is greater than 5')
 elif ((number % 10) == 0):
-    print(f'{remainder} and is 0')
-elif ((number % 10) < 6) and (number % 10):
-    if (remainder < 0):
-        print(f'-{remainder} and is less than 6 and not 0')
-    else:
-        print(f'{remainder} and is less than 6 and not 0')
+    print(f'{remainder: d} and is 0')
+elif (remainder < 6) and (remainder != 0):
+    print(f'{remainder: d} and is less than 6 and not 0')
