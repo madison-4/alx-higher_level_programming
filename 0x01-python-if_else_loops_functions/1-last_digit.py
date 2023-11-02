f313ecc642b7975a3eb4381d9d2b8e35ff560cae
nox <samingosi97@gmail.com>
2023-11-01 19:30:22 -0700

    use format in place of fstrings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,10 @@ if (number < 0):
     remainder = number % 10
     remainder = -remainder
     number = -number
-print(f'Last digit of {number: d} is', end=' ')
+print(f'Last digit of {number:d} is', end=' ')
 if ((number % 10) > 5):
     print(f'{remainder:d} and is greater than 5')
 elif ((number % 10) == 0):
-    print(f'{remainder: d} and is 0')
+    print(f'{remainder:d} and is 0')
 elif (remainder < 6) and (remainder != 0):
     print(f'{remainder: d} and is less than 6 and not 0')
