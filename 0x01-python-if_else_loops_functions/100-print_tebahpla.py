a3ba60cffe6f7100c5ba83900f59f259134fcc77
nox <samingosi97@gmail.com>
2023-11-02 12:30:45 -0700

    print letters alternating

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,6 +3,6 @@
 for i in range(0, 26):
     temp = ord('z') - i
     if ((i % 2) == 0):
-        print(chr(temp), end="")
+        print("{}".format(chr(temp)), end="")
     if ((i % 2) == 1):
-        print(chr(temp - 32), end="")
+        print("{}".format(chr(temp - 32)), end="")
