529d649ff201c743217afeca34e25d9254fc1598
nox <samingosi97@gmail.com>
2023-11-02 12:48:13 -0700

    remove enforcment for negative indices

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,5 +3,7 @@
 # of course, since strings are immutable
 # a new string will be returned
 def remove_char_at(str, n):
-    newstr = str[:n] + str[(n + 1):]
-    return (newstr)
+    if (n >= 0):
+        newstr = str[:n] + str[(n + 1):]
+        return (newstr)
+    return (str)
