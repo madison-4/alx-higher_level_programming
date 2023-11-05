dd67bd3442a4977945a20b6577a8fb7aa5892ec1
nox <samingosi97@gmail.com>
2023-11-05 06:04:39 -0800

    use dir to find defined names in module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     import imp
     module = imp.load_compiled('hidden_4', 'hidden_4.pyc')
     names = dir(module)
-    names = filter(lambda x: not x.startswith('__'), names)
     names = names.sort()
     for name in names:
+        if (name[:2] == __):
+            continue
         print(name)
