70fd94ca618b0865563db06f07c7db6b71b86e4c
nox <samingosi97@gmail.com>
2023-11-05 06:53:49 -0800

    make simple calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Make a simple calculator
+# It is similar to the one we used
+
+if __name__ == "__main__":
+    import sys
+    import calculator_1
+    if ((len(sys.argv)) != 4):
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
