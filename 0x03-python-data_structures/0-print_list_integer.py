c4d42d2b857140d3e6be0aaf2378f1781b1ce9d2
nox <samingosi97@gmail.com>
2023-11-05 11:48:47 -0800

    print ints in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# print ints in a list
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        if type(i) is int:
+            print("{:d}".format(i))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+# About
+This is about the first foray into python DSA with lists and tuples
