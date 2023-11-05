8b55dc0ee34ab017d714eeb3b64aa19ef8ebc3e5
nox <samingosi97@gmail.com>
2023-11-05 01:49:04 -0700

    don't output colon with 0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,12 +5,10 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv[1:])
     if (length == 0):
-        print("{:d}: arguments.".format(length))
-        sys.exit()
+        print("{:d} arguments.".format(length))
     elif (length == 1):
         print(f"{length:d} argument:")
         print(f"1: {sys.argv[1]}")
-        sys.exit()
     else:
         print(f"{length:d} arguments:")
         for i in range(1, (length + 1)):
