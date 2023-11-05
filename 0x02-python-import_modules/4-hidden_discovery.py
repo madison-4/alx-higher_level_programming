0a8d2d0420a5c3167e9fe10a4ee31ac9cdb82219
nox <samingosi97@gmail.com>
2023-11-05 06:05:57 -0800

    import it like normal .py files

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # get the variables names from a compiled file
 if __name__ == "__main__":
-    import sys
+    import hidden_4
     import imp
-    module = imp.load_compiled('hidden_4', 'hidden_4.pyc')
     names = dir(module)
     names = names.sort()
     for name in names:
