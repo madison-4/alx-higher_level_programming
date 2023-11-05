7c62879934895582b47892274918ecd139230064
nox <samingosi97@gmail.com>
2023-11-05 12:11:01 -0800

    rename function

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # retrieve an element from a list
-def element_at(my_list, idx, element):
+def replace_in_list(my_list, idx, element):
     if (idx < 0):
         return (my_list)
     length = len(my_list)
     if (idx >= length):
         return (my_list)
     my_list[idx] = element
+    return (my_list)
