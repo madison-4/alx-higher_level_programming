7ea99a5adf1829d91e2dbdbcacf38563eb288f7a
nox <samingosi97@gmail.com>
2023-11-05 05:56:27 -0800

    use dir to find defined names in module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # get the variables names from a compiled file
 if __name__ == "__main__":
-    import hidden4
-    names = dir(hidden4)
+    import sys
+    import imp
+    module = imp.load_compiled('hidden_4', 'hidden_4.pyc')
+    names = dir(module)
     names = filter(lambda x: not x.startswith('__'), names)
-    names= names.sort()
+    names = names.sort()
     for name in names:
         print(name)
