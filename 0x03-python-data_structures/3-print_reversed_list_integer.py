78578d3fc2467d7c5a6f6143884f4216223f09df
nox <samingosi97@gmail.com>
2023-11-05 18:53:03 -0800

    account for empty lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # print ints in a list in reverse
 def print_reversed_list_integer(my_list=[]):
-    new_list = my_list[::-1]
-    for i in new_list:
-        print("{:d}".format(i))
+    if my_list:
+        new_list = my_list.copy()
+#        new_list.reverse()
+        for i in reversed(new_list):
+            print("{:d}".format(i))
