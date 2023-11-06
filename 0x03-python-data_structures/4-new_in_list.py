174db9cf80cb45da2b9b32527a3f5fbc79963ec1
nox <samingosi97@gmail.com>
2023-11-05 21:43:03 -0800

    replace int elem

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# retrieve an element from a list
+def replace_in_list(my_list, idx, element):
+    if (idx < 0):
+        return (my_list.copy())
+    length = len(my_list)
+    if (idx >= length):
+        return (my_list.copy())
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return (new_list)
