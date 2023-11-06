d2c4aeb41cfaea0451d9d7cccef69fae4f519472
nox <samingosi97@gmail.com>
2023-11-06 01:45:22 -0800

    return original fior emoty

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,7 +9,7 @@ def no_c(my_string):
             if (letter == 'C' or letter == 'c'):
                 looplist.remove(letter)
         for let in looplist:
-            new_str += let
+            new_str += str(let)
         return (new_str)
     else:
         return (my_string)
