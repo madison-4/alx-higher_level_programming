f8d6dea59ff3f7e3c8d4f7e80579e453d5d43f50
nox <samingosi97@gmail.com>
2023-11-06 03:09:37 -0800

    pycodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,16 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 # remove the letter c from the string
+
+
 def no_c(my_string):
     if my_string:
         str_list = list(my_string)
         new_str = ''
         looplist = str_list.copy()
         for letter in looplist:
-            if (letter == 'C' or letter == 'c'):
+            if (letter == 'C'):
+                looplist.remove(letter)
+            if (letter == 'c'):
                 looplist.remove(letter)
         for let in looplist:
             new_str += str(let)
