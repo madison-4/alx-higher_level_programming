079a5b316d9092e76109c654a160a9205667652a
nox <samingosi97@gmail.com>
2023-11-05 21:52:09 -0800

    replace int elem

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# remove the letter c from the string
+def no_c(my_string):
+    remstr = my_string.copy()
+    for i in remstr:
+        if (i == 'c'):
+            remstr.pop(i)
