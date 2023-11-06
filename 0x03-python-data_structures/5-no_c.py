c78009179d92e131fb17a7441617c8ae7cf7c082
nox <samingosi97@gmail.com>
2023-11-06 01:37:23 -0800

    remove comments and test

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,12 +3,10 @@
 def no_c(my_string):
     if my_string:
         str_list = list(my_string)
-        new_str = ''
         print(str_list)
         looplist = str_list.copy()
         for letter in looplist:
             if (letter == 'C' or letter == 'c'):
                 looplist.remove(letter)
-        for char in looplist:
-            new_str += char
+        new_str = ''.join([str(x) for x in looplist])
         return (new_str)
