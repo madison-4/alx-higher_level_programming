06eb083e34b3275341a4bbe403bd5a37ee82e067
nox <samingosi97@gmail.com>
2023-11-06 15:39:00 -0800

    attempt to modify tupkes

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,19 +3,17 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum_list = []
-    for i in range(0, 2):
-        if (len(tuple_a) < 2):
-            r = len(tuple_a)
-            if (r == 0):
-                tuple_a = (0, 0)
-            else:
-                tuple_a += (0,)
-            if ((len(tuple_b)) < 2):
-                if ((len(tuple_b)) == 0):
-                    tuple_b = (0, 0)
-                else:
-                    tuple_b += (0,)
-        sum_list.append(tuple_a[i] + tuple_b[i])
-        sum_list = tuple(sum_list)
-    return (sum_list)
+    if (len(tuple_a) < 2):
+        r = len(tuple_a)
+        if (r == 0):
+            tuple_a = (0, 0)
+        else:
+            tuple_a += (0,)
+    if ((len(tuple_b)) < 2):
+        if ((len(tuple_b)) == 0):
+            tuple_b = (0, 0)
+        else:
+            tuple_b += (0,)
+    sum_list = sum(tuple_a[0], tuple_b[0])
+    sum_list2 = sum(tuple_a[1], tuple_b[2])
+    return (sum_list, sum_list2)
