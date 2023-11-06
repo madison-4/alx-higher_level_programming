ece58feac0d3c8d8aec6abefa58f906c4e8d3d36
nox <samingosi97@gmail.com>
2023-11-05 23:22:53 -0800

    replace with empty for later review

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 # remove the letter c from the string
 def no_c(my_string):
-    remstr = my_string.copy()
-    for i in remstr:
-        if (i == 'c' or i == 'C'):
-            remstr.pop(i)
+    if my_string:
+        cap_list = ''
+        new_str = ''
+        str_list = my_string.split('c')
+        print(str_list)
+        for i in str_list:
+            if (i == 'c'):
+                new_str += ''
+            else:
+                new_str += i
+        for car in new_str:
+            if (car == 'C'):
+                cap_list += ''
+            else:
+                cap_list += car
+        print(cap_list)
+no_c("C c is cfun not c too")
