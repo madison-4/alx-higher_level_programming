59acb9ba6834692b1e84d217c479c1fa13e27ce9
nox <samingosi97@gmail.com>
2023-11-06 03:24:03 -0800

    use append

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,14 +6,12 @@ def no_c(my_string):
     if my_string:
         str_list = list(my_string)
         new_str = ''
-        looplist = str_list.copy()
-        for letter in looplist:
-            if (letter == 'C'):
-                looplist.remove(letter)
-            if (letter == 'c'):
-                looplist.remove(letter)
+        looplist = []
+        for letter in str_list:
+            if ((letter != 'C') and (letter != 'c')):
+                looplist.append(letter)
         for let in looplist:
-            new_str += new_str.append(let)
+            new_str += let
         return (new_str)
     else:
         return (my_string)
