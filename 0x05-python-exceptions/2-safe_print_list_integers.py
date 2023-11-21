9e2bb1dce1e482124754758e5c44d51368776dba
nox <samingosi97@gmail.com>
2023-11-20 17:40:22 -0800

    account for name error value and typeerror

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             num += 1
-        except (NameError, IndexError, TypeError):
+        except (NameError, IndexError, TypeError, ValueError):
             continue
     print()
     return (num)
