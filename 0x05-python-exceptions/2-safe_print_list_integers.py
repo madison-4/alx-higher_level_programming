8fa8ac068f011bd8c0c6eb630efbe8581570c74a
nox <samingosi97@gmail.com>
2023-11-20 17:49:18 -0800

    value index and type error added

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end='')
             num += 1
-        except:
+        except (ValueError, TypeError, NameError, IndexError):
             continue
     print()
     return (num)
