e0d80ec239125c87ba60c0b020749ce29a2703c2
nox <samingosi97@gmail.com>
2023-11-29 22:30:03 -0800

    add all unique nubers

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This function adds all unique integers in a list
+Am going to use the set function
+"""
+def uniq_add(my_list=[]):
+    r = set(my_list)
+    su = 0
+    for num in r:
+        su += num
+    return (su)
