cb4bced5d59c73232e401a77d1e9fef5b4cce417
nox <samingosi97@gmail.com>
2023-11-29 22:00:21 -0800

    delete all emacs generated fles

--- 0x04-python-more_data_structures/testone.py
+++ 0x04-python-more_data_structures/testone.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-search_replace = __import__('1-search_replace').search_replace
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
 
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-print(new_list)
-print(my_list)
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
