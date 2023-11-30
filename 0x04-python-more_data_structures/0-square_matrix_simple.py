23848a4ffd5baf513e5bbc88b1d3f7813be12b21
nox <samingosi97@gmail.com>
2023-11-29 21:45:54 -0800

    use nested lists comprehensions

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,8 +2,4 @@
 # A function that computes the value of a square matric of all ints
 # the function prints a sqaure matrix
 def square_matrix_simple(matrix=[]):
-    res = []
-    for row in matrix:
-        newrow = [x**2 for x in row]
-        res.append(newrow)
-    return (res)
+    return ([[elem**2 for elem in matrix[i]] for i in range(len(matrix))])
