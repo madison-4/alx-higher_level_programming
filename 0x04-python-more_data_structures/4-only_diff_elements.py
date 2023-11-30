9f178219570d3d19b0088cdf66244f25214258bf
nox <samingosi97@gmail.com>
2023-11-29 23:27:34 -0800

    remove test files

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,15 +1,14 @@
+
+
 #!/usr/bin/python3
 # get common elements in only one list
 # I dislike this brute force method
 def only_diff_elements(set_1, set_2):
     ret = []
     for elem in set_1:
-        valret = 0
-        for el in set_2:
-            if (elem == el):
-                valret = 1
-                break
-            ret.append(el)
-        if (valret == 0):
+        if elem not in set_2:
             ret.append(elem)
+    for el in set_2:
+        if el not in set_1:
+            ret.append(el)
     return (set(ret))
