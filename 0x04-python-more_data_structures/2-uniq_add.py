fd51ad4e6514bf5110b37147724116314cfdfd98
nox <samingosi97@gmail.com>
2023-11-29 22:31:12 -0800

    add all unique nubers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+
+
 """
 This function adds all unique integers in a list
 Am going to use the set function
 """
+
+
 def uniq_add(my_list=[]):
     r = set(my_list)
     su = 0
