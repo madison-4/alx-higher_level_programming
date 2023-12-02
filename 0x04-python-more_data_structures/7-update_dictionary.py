8955bebbd84927cc3bcc8668ce2d076afa3f74e7
nox <samingosi97@gmail.com>
2023-12-02 10:48:27 -0800

    pycodestyle fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # update dictionaries
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {key : value}
+    new_dict = {key: value}
     a_dictionary.update((new_dict))
     return (a_dictionary)
