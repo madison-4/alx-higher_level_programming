677225060aec448fb8f70007fe82750b1f9ba3f4
nox <samingosi97@gmail.com>
2023-12-02 10:43:32 -0800

    use a new duct

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # update dictionaries
 def update_dictionary(a_dictionary, key, value):
-    new_dic = {key : value}
-    return (dict(a_dictionary.update(new_dic)))
+    new_dict = {key : value}
+    a_dictionary.update((new_dict))
+    return (a_dictionary)
