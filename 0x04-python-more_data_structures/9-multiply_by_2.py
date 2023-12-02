d64ed74ee2ccf492e4ec2144139293b750973eb5
nox <samingosi97@gmail.com>
2023-12-02 12:54:27 -0800

    use pycodestyle

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,5 +4,5 @@ def multiply_by_2(a_dictionary):
     rect = [ke for ke in a_dictionary.keys()]
     val = [va for va in a_dictionary.values()]
     vals = [x * 2 for x in val]
-    res = {rect[i]:vals[i] for i in range(len(rect))}
+    res = {rect[i]: vals[i] for i in range(len(rect))}
     return (res)
