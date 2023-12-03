11df775a50d7ee4dff7128885bc06397d3d30f86
nox <samingosi97@gmail.com>
2023-12-03 08:15:20 -0800

    use while loop and no range function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,17 @@
 # convert to arabic numbers from roman numbers
 def roman_to_int(roman_string):
     romint = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,'D': 500, 'M': 1000}
-    value = 0
+    s, value = 0, 0
     place1 = 0
     if not roman_string:
         return 0
-    for s in range(len(roman_string)):
+    while (s < (len(roman_string))):
         value1 = romint.get(roman_string[s])
         if ((s + 1) < len(roman_string)):
             value2 = romint.get(roman_string[s + 1])
             if (value1 >= value2):
                 value += value1
+                s += 1
                 continue
             else:
                 value += (value2 - value1)
@@ -19,5 +20,6 @@ def roman_to_int(roman_string):
                 continue
         else:
             value += value1
+            s += 1
             continue
     return (value)
