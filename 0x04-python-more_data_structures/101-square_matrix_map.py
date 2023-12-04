ee3d043ad246b71d7d84f3770cb0333ba2a8d7ec
nox <samingosi97@gmail.com>
2023-12-03 22:29:19 -0800

    use solely map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return ([list(map(lambda x: x ** 2, matrix[i])) for i in range(len(matrix))])
+  return list(map(lambda row: list(map(lambda element: element**2, row)), matrix))
