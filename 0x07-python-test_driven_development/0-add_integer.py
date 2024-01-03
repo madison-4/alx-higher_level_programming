e02ff67dd7d323c6598f0c2896321a674f1cf119
nox <samingosi97@gmail.com>
2024-01-03 11:58:18 -0800

    use the test in the docstrings

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,34 @@ def add_integer(a, b=98):
     """A simple addition function
     It adds two integers and returns the results
     Some tests here
-
+    # Test for function of addition
+    # The tests should ensure that the inputs are correct and the output.
+    # ideally they should ensure the function works as intended
+    
+    >>> add_integer(3, b)
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integer
+    >>> add_integer(a, 3)
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    >>> add_integer({56, 67}, 67)
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    >>> add_integer(56, {98, 76})
+    Traceback (most recent call last):
+    ...
+    TypeError: b must be an integer
+    >>> add_integer('str', 'stry')
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
+    >>> add_integer([90, 89], 87)
+    Traceback (most recent call last):
+    ...
+    TypeError: a must be an integer
     """
     
     if (type(a) is not int and type(a) is not float):
