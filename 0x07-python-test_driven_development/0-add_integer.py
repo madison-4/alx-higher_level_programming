be890135f62383966d406b24cecd2002f28eeddd
nox <samingosi97@gmail.com>
2024-01-03 21:49:42 -0800

    tests addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -35,6 +35,11 @@ def add_integer(a, b=98):
     TypeError: a must be an integer
     """
     
+    try: a
+    except: NameError
+    raise TypeError("a must be an integer")
+    try: b
+    except: raise TypeError("b must be an integer")
     if (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
     if (type(b) is not int and type(b) is not float):
