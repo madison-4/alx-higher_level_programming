68b25d1939c21ca6f643a86d4134c74e2b93bfd4
nox <samingosi97@gmail.com>
2024-01-04 12:11:07 -0800

    remove test ases

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-# a modeule for the python task
 
 class Rectangle:
-    """This is a class that defines a rectangle,
-    It is used to create other rectangels
-    """
-    def __init__(self):
-        pass
     pass
