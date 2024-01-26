454e82adf973f276c9b361588b36e63f6aca2fdd
nox <samingosi97@gmail.com>
2024-01-26 13:30:01 -0800

    fix pycodestye

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,11 +2,11 @@
 """This module is for  is for making linked lists in python
 """
 
-# an attempt at making a singly lunked list
+
 class Node:
     """ This is a class for a node of a singly linked list
     """
-    
+
     def __init__(self, data, next_node=None):
         """This function is to initualize the node class
         """
@@ -20,33 +20,34 @@ class Node:
     def data(self):
         """This is a getter for the data variable
         """
-        
+
         return (self.__data)
 
     @data.setter
     def data(self, value):
         """set the data value to value
         """
-        
+
         if (type(value) is not int):
             raise TypeError("data must be an integer")
         self.__data = value
-        
+
     @property
     def next_node(self):
         """A getter for the next node class
         """
-        
+
         return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
         """A setter for the next node
         """
-        
+
         if ((type(value) not in [None, Node])):
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """the linked list class
@@ -84,14 +85,14 @@ class SinglyLinkedList:
                 return
             new.next_node = temp
             prev.next_node = new
-            
+
     def __str__(self):
         """dunder method to print list
         """
-        
+
         temp = self.head
         out = ""
-        
+
         while (temp is not None):
             out += str(temp.data) + "\n"
             temp = temp.next_node
