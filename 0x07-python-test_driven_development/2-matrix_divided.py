620f0f46fb6d12c075650c34017872d23ae26b98
nox <samingosi97@gmail.com>
2024-02-14 03:28:49 -0800

    remove emacs generated files

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This module seesk to divvide a matrix then return the divide matrix
+    The matrix should be of rows of equal size
+    ::
+    It simply defines a function
+"""
+
+
+def matrix_divided(matrix, div):
+    """This is teh function to divide a mtraix
+    """
+
+
+    if (div == 0):
+        raise ZeroDivisionError('division by zero')
