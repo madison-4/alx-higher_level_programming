d5fab1f143f99b08c7c3e3e578b35cd7cf4782cd
nox <samingosi97@gmail.com>
2024-02-14 21:17:33 -0800

    all tests passed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,6 +11,10 @@ def matrix_divided(matrix, div):
     """
 
 
+    if (type(div) is not int and type(div) is not float):
+        raise TypeError('div must be a number')
+    if (div == 0):
+        raise ZeroDivisionError('division by zero')
     if (type(matrix) is not list):
         raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     if (type(matrix[0]) is not list):
@@ -24,10 +28,6 @@ def matrix_divided(matrix, div):
         for r in i:
             if (type(r) not in [int, float]):
                 raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    if (type(div) not in [int, float]):
-        raise TypeError('div must be a number')
-    if (div == 0):
-        raise ZeroDivisionError('divisin by zero')
     ans = []
     for row in matrix:
         rows = []
