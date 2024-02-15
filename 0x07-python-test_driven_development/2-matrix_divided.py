3896ce6887d722d3e76e83b7a392ce28d92e9a15
nox <samingosi97@gmail.com>
2024-02-14 17:23:11 -0800

    make function

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -11,5 +11,29 @@ def matrix_divided(matrix, div):
     """
 
 
+    if (type(matrix) is not list):
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if (type(matrix[0] is not list):
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    length = len(matrix[0])
+    for i in matrix:
+        if (type(i) is not list):
+            raise TypeError('matrix must be a maatrix (list of lists) of integers/floats')
+        if (len(i) != length):
+            raise TypeError('Each row of the matrix must have the same size')
+        for r in i:
+            if (type(r) not in [int, float]):
+                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+    if (type(div) not in [int, float]):
+        raise TypeError('div must be a number')
     if (div == 0):
-        raise ZeroDivisionError('division by zero')
+        raise ZeroDivisionError('divisin by zero')
+    ans = []
+    for row in matrix:
+        rows = []
+        for elem in row:
+            elem = elem / div
+            elem = round(elem, 2)
+            rows.append(elem)
+        ans.append(rows)
+    return (ans)
