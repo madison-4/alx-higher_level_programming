7074cf916373ee87163c38c890dd5668d0eaaef5
nox <samingosi97@gmail.com>
2024-02-17 03:29:09 -0800

    only the cirrect case fails

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+"""A moduel to modify a strig based on special characters
+"""
+
+
+def text_indentation(text):
+    """function to print newline after the special chars
+    """
+
+    if (type(text) is not str):
+        raise TypeError('text must be a string')
+    for e in text:
+        if (e in ['.', '?', ':']):
+            print(e, end='')
+            print()
+            print()
+        else:
+            print(e)
