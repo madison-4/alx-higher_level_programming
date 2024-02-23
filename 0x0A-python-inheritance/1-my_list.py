3f8a7b6602fdae01a1ff14aa1225d9b3bb70497d
nox <samingosi97@gmail.com>
2024-02-23 08:46:42 -0800

    av forgotten all about lasses

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-""" A function to define a class that inherits from the lists class
+""" A function to define a class that inherits the lists class, list is a base class of it
 """
 
 
 class Mylist(list):
-    """ defines a list that ineriots from int
+    """ defines a list that ineriots int
     """
 
     def __init__(self):
-        
+        """ costructor for the method
+        """
+
+        self.__new = []
+
+    @property
+    def 
