3396c8f1773c890f7aeb5c6f2efeb9edfd28d7f6
nox <samingosi97@gmail.com>
2024-03-05 10:17:48 -0800

    Inherit and print custom kessage

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A module to ineherit from the Base Geometry class
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ A class to make a simple rectangle
+        It inherits from the ase geomrtery for simple uses
+    """
+
+    def __init__(self, width, height):
+        """ Initialization function
+            Args:
+                 width - rectangle width
+                 height - height of the rectangle
+        """
+
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ Implementte area method
+        """
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """ What to print following str method
+        """
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
