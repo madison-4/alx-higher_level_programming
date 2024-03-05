6f7b92e35259d2ce5f0335cbeb3b757bc287339b
nox <samingosi97@gmail.com>
2024-03-04 18:10:23 -0800

    get instace usig types

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module to get the isinancefunction
+"""
+
+
+def is_same_class(obj, a_class):
+    """clones is instance
+    """
+
+    if (type(obj) is a_class):
+        return (True)
+    else:
+        return (False)
