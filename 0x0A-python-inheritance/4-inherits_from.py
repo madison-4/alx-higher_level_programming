97ac3e8bebf64fe967e6c16acf418e09ea953f5c
nox <samingosi97@gmail.com>
2024-03-05 00:25:43 -0800

    fig oyt lat

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Module to check an object is an instance of a given class
+It also checks that it's an instance of a class that inherits the given
+"""
+
+
+def inherits_from(obj, a_class):
+    """ function to check it inherits otherwise it gives fals
+    """
+
+    return (isinstance(obj, a_class))
