c4e146c99ea55ab0c1327c6c529e849644ce3d43
nox <samingosi97@gmail.com>
2024-03-06 11:19:35 -0800

    docstrings b4 import starement

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import json
 """ The module to return a json representation of an object
 """
 
 
+import json
 def to_json_string(my_obj):
     """ Function to convert oject to json string
         The function does not account for any execpetions
