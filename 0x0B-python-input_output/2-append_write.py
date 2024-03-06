2336630091fabc40ff56fe159923cfcfbdc7a858
nox <samingosi97@gmail.com>
2024-03-06 11:02:10 -0800

    aped

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Mosule to write to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """ Function to write to files
+         Args:
+              filename: file path to write to
+              text: text to write to gicen file
+    """
+
+    with open(filename, mode='a', encoding="utf-8") as fd:
+        return (fd.write(text))
