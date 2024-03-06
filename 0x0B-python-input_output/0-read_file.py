01cec69eab4c46a271cc9d2be38d5f64a057531a
nox <samingosi97@gmail.com>
2024-03-05 17:13:03 -0800

    ue print to get stdout without importing a module

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" This modulereads a text file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """ function to read the file and print it to stdout
+    """
+
+    with open(filename, mode='r', encoding='utf-8') as filse:
+        data = filse.read()
+    print(data)
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+# About
+This is about reading and writing from files
+It mes use of te official python docs as a awhe
