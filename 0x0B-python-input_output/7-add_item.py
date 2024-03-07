02227d6156d089bcb621b3d121874ace4c484b8c
nox <samingosi97@gmail.com>
2024-03-07 09:55:41 -0800

    more docstrings

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
 with open('add_item.json', 'w', encoding="utf-8") as fildes:
+    """ Use with so te context automatocally closes
+    """
     try:
         form = load_from_json_file('add_item.json')
     except Exception as e:
