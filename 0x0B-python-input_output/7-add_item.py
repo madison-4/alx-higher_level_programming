6233de889165f1da9995afcf4fa6e3cc93cabaa8
nox <samingosi97@gmail.com>
2024-03-07 09:57:45 -0800

    hfgucg,

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 """ A module to add all arguments to a python list
+    It uses previous moduels
 """
 
 
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+""" get the loading function
+"""
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+""" Function to save the object to a file
+"""
+
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
 with open('add_item.json', 'w', encoding="utf-8") as fildes:
     """ Use with so te context automatocally closes
     """
+
     try:
         form = load_from_json_file('add_item.json')
     except Exception as e:
