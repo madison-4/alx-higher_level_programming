5ac9e5ed1ebbe4af94b4fef80a6593a0f2e1a8dd
nox <samingosi97@gmail.com>
2024-03-06 22:52:18 -0800

    want toreste head

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
-fildes = open(add_item.json, mode='a', encoding="utf-8")
-save_to_json_file(ls, add_item.json)
+fildes = open('add_item.json', mode='a', encoding="utf-8")
+save_to_json_file(ls, fildes)
 string = load_from_json_file(add_item.json)
 filds.close()
