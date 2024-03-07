7f03bd7588c0143f4dee2548d32a005e9f678692
nox <samingosi97@gmail.com>
2024-03-07 09:53:38 -0800

    use try yo check file existsence[5~

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,16 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
-with open('add_item.json', 'a+', encoding="utf-8") as fildes:
-    form = load_from_json_file('add_item.json')
-    form.extend(ls)
-    save_to_json_file(form, 'add_item.json')
+with open('add_item.json', 'w', encoding="utf-8") as fildes:
+    try:
+        form = load_from_json_file('add_item.json')
+    except Exception as e:
+        check = e
+    if (e is not None):
+        form = []
+        form.extend(ls)
+        save_to_json_file(form, 'add_item.json')
+    else:
+        form = load_from_json_file('add_item.json')
+        form.extend(ls)
+        save_to_json_file(form, 'add_item.json')
