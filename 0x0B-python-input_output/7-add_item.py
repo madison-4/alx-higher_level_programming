6c878cff080cbedefb7623d48476b03117ee0010
nox <samingosi97@gmail.com>
2024-03-07 03:37:57 -0800

    style of code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,9 +3,9 @@
 """
 
 
+import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-import sys
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
 fildes = open('add_item.json', 'a', encoding="utf-8")
 form = load_from_json_file('add_item.json')
