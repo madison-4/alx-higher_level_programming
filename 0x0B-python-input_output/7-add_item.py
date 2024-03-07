1679f8d36050a8265817ce198b2f757eddf6935e
nox <samingosi97@gmail.com>
2024-03-07 03:41:31 -0800

    use with cintext manager

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,7 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
-fildes = open('add_item.json', 'a+', encoding="utf-8")
-form = load_from_json_file('add_item.json')
-form.extend(ls)
-save_to_json_file(form, 'add_item.json')
-fildes.close()
+with open('add_item.json', 'a+', encoding="utf-8") as fildes:
+    form = load_from_json_file('add_item.json')
+    form.extend(ls)
+    save_to_json_file(form, 'add_item.json')
