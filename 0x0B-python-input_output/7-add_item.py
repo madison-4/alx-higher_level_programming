b30ad62809898611d3ca135bb980d622c01c2a78
nox <samingosi97@gmail.com>
2024-03-07 03:35:51 -0800

    append gicven lists

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
 ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
-fildes = open('add_item.json', mode='a', encoding="utf-8")
-save_to_json_file(ls, fildes)
-string = load_from_json_file(add_item.json)
-filds.close()
+fildes = open('add_item.json', 'a', encoding="utf-8")
+form = load_from_json_file('add_item.json')
+form.extend(ls)
+save_to_json_file(form, 'add_item.json')
+fildes.close()
