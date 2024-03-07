8ba1f957a2d38654e4d971ef09299ebfd601aecf
nox <samingosi97@gmail.com>
2024-03-06 22:08:44 -0800

    initial commit, Am yet to ceate thr fie automatically

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,3 +6,8 @@
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
+ls = [sys.argv[i] for i in range(1, (len(sys.argv)))]
+fildes = open(add_item.json, mode='a', encoding="utf-8")
+save_to_json_file(ls, add_item.json)
+string = load_from_json_file(add_item.json)
+filds.close()
