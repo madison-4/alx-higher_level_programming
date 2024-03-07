e1f47be7e8828e12b9a71a2edaefeaa04d47df63
nox <samingosi97@gmail.com>
2024-03-06 21:26:36 -0800

    make it up to date

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A module to add all arguments to a python list
+"""
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
