f43c552a54aa5cda64f7aae3119826905b6b82d3
nox <samingosi97@gmail.com>
2024-03-17 11:32:52 -0700

    upload to work from lap

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,32 @@ class Rectangle(base.Base):
             Args:
                   width: private isntance attribute, width of rectangle
                   height: height of the rectangle
+                  x: private isntance attribute showing x point
+                  y: point on rectangle
+                  id of the rectangle object
+        """
+
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ getter for the wisth class
+        """
+
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ A setter for the width value
+        """
+
+        self.__width = value
+        return (self.__width)
+
+    @property
+    def height
+    
