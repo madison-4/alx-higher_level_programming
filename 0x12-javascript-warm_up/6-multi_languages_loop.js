61487ceef09977117f9cd95e286edfcde4cb3a92
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 11:54:07 +0300

    semis

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // a script to print multiple lamguages
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; i++)
-{
-    console.log(languages[i]);
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
 }
