d3cb86cc942cd8fab5c65dede8fadbcd1cf0f870
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 21:43:46 +0300

    semistd

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // a function that is exported outside the module
 
-exports.add = function(a, b) {
-    return (parseInt(a) + parseInt(b));
-}
+exports.add = function (a, b) {
+  return (parseInt(a) + parseInt(b));
+};
