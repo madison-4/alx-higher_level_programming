d9f67416a058b78ad19a9efb62128f00df7b1e2b
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 10:08:00 +0300

    use standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // a script to get commandline arguments
 
-let arrayCmd = process.argv;
+const arrayCmd = process.argv
 
 if ((arrayCmd.length === 2)) {
-    console.log('No argument');
-} else if (( arrayCmd.length === 3 )) {
-    console.log('Argument found');
+  console.log('No argument')
+} else if ((arrayCmd.length === 3)) {
+  console.log('Argument found')
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found')
 }
