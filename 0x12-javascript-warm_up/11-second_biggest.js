492f5798ae2f6ec5db2bb6099cfe3199ec16fa5b
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 21:14:13 +0300

    use semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,37 +1,31 @@
 #!/usr/bin/node
 // a script to find the second biggest number in an array
 
-let original = process.argv;
+const original = process.argv;
 
-function second(argum) {
-    let largest = argum[0];
-    let sec = -Infinity;
+function second (argum) {
+  let largest = argum[0];
+  let sec = -Infinity;
 
-    for (let i = 0; i < argum.length; i++)
-    {
-	if (argum[i] > largest)
-	{
-	    sec = largest;
-	    largest = argum[i];
-	} else if ((argum[i] > sec) && (argum[i] !== largest))
-	{
-	    sec = argum[i];
-	}
+  for (let i = 0; i < argum.length; i++) {
+    if (argum[i] > largest) {
+      sec = largest;
+      largest = argum[i];
+    } else if ((argum[i] > sec) && (argum[i] !== largest)) {
+      sec = argum[i];
     }
-    return (sec);
+  }
+  return (sec);
 }
-
 
 if (original.length === 2) {
-    console.log(0);
-    process.exit();
+  console.log(0);
+  process.exit();
 } else if (original.length === 3) {
-    console.log(1);
-    process.exit();
+  console.log(0);
+  process.exit();
 } else {
-    let placehold = original.slice(2);
-    let current = placehold.map(Number);
-    console.log(second(current));
+  const placehold = original.slice(2);
+  const current = placehold.map(Number);
+  console.log(second(current));
 }
-    
-    
