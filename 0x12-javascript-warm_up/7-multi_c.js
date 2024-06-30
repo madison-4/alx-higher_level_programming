17b5e14a2fcedd9874706a901fadb6e3401d64f8
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 13:02:42 +0300

    use for loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// a script to print multiple lamguages
+
+const rep = process.argv[2];
+const num = parseInt(rep);
+
+if (isNaN(num) || (num < 0)) {
+  console.log('Missing number of occurrences');
+  process.exit();
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+}
