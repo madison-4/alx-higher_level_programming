3c44192e7b7e16a5069a4dc9abbbef5bc9420598
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 11:36:55 +0300

    use for loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// a script to print multiple lamguages
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+
+for (let i = 0; i < languages.length; i++)
+{
+    console.log(languages[i]);
+}
