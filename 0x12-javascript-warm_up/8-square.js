90828e90683698509d6ad7d9173df51ea0c985c3
noxcodes <70919949+madison-4@users.noreply.github.com>
2024-06-30 13:21:17 +0300

    .blh

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
-// a script to print multiple lamguages
+// a script to print a sqaure
 
 const rep = process.argv[2];
 const num = parseInt(rep);
 let word = '';
 
 if (isNaN(num)) {
-    console.log('Missing size');
+  console.log('Missing size');
   process.exit();
 } else {
-    for (let i = 0; i < num; i++) {
-	word= '';
-	for (let j = 0; j < num; j++) {
-	    word += 'X'
-	}
-	console.log(word);
+  for (let i = 0; i < num; i++) {
+    word = '';
+    for (let j = 0; j < num; j++) {
+      word += 'X';
+    }
+    console.log(word);
   }
 }
