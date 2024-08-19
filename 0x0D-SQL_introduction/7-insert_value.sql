7b58087fa67988abd94279c809f70f7acf8a7f2f
noxcodes <samingosi97@gmail.com>
2024-08-19 12:37:56 +0300

    insert riows using insert

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,8 @@
+-- This script inserts a new row into the first table
+-- table in the hbtn database.
+-- The database is passed as an argument to mysql
+
+INSERT INTO 'first_table' (
+       id = "89",
+       name = "Best School"
+       );
