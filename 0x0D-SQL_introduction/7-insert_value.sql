4c6d7658be2eee88c7d347eea4a2963ecd22587a
noxcodes <samingosi97@gmail.com>
2024-08-19 12:48:05 +0300

    remove quotes from table name

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -2,5 +2,5 @@
 -- table in the hbtn database.
 -- The database is passed as an argument to mysql
 
-INSERT INTO 'first_table' (id, name)
-       VALUES (89, "Best School");
+INSERT INTO first_table (id, name)
+       VALUES (89, 'Best School');
