1aa656cdb1c4ca76034b5db5e2c80281710188d7
noxcodes <samingosi97@gmail.com>
2024-08-19 11:05:32 +0300

    create a tabke if it doesn't exist

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+--  a script to create a table in the current database
+-- It creates the table only if the table doesn't already exist
+
+CREATE TABLE first_table (
+       id INT,
+       name VARCHAR(256)
+ );
