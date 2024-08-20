b6447a9afe3c68f458e0dc4c83d8fc49e747b92b
noxcodes <samingosi97@gmail.com>
2024-08-20 10:30:11 +0300

    explicitly use the db

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- A script to create the database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `cities` (
        `id`    	  INT		NOT NULL UNIQUE AUTO_INCREMENT,
        `state_id` INT 		NOT NULL,
        `name`     VARCHAR(256)  NOT NULL,
