86924ca2b9b952e9c031c6700c41620352d148ec
noxcodes <samingosi97@gmail.com>
2024-08-20 09:22:50 +0300

    get prinmary band foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- A script to create the database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
+       `id`    	  INT		NOT NULL UNIQUE AUTO_INCREMENT,
+       `state_id` INT 		NOT NULL,
+       `name`     VARCHAR(256)  NOT NULL,
+       PRIMARY kEY(`id`),
+       FOREIGN KEY(`state_id`)
+       REFERENCES `hbtn_0d_usa`.`state`(`id`)
+       );
