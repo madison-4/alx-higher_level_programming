63f3b30cc2f198fb68c43d8f5c25da2c78af6983
noxcodes <samingosi97@gmail.com>
2024-08-20 10:12:00 +0300

    jbsvl

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
        `id`    	  INT		NOT NULL UNIQUE AUTO_INCREMENT,
        `state_id` INT 		NOT NULL,
        `name`     VARCHAR(256)  NOT NULL,
-       PRIMARY kEY(`id`),
+       PRIMARY KEY(`id`),
        FOREIGN KEY(`state_id`)
        REFERENCES `hbtn_0d_usa`.`state`(`id`)
        );
