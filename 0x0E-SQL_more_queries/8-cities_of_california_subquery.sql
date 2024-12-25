dd3b2ce85c6a3da8564a5883192ac3e7d14e7347
noxcodes <samingosi97@gmail.com>
2024-12-25 12:44:27 +0300

    use quiotes instead of backtivks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,10 @@
 -- Lists all cities of CA in the database hbtn_0d_usa.
 -- Results are ordered by ascending cities.id.
 SELECT `id`, `name`
-  FROM `cities`
- WHERE `state_id` IN (
+FROM `cities`
+WHERE `state_id` IN (
        SELECT `id`
        FROM `states`
-       WHERE `name`=`California`
+       WHERE `name`="California"
 )
  ORDER BY `id`;
