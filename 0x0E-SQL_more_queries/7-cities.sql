00d4147b4abbf54b27b83e1078c1333b94b018a3
noxcodes <samingosi97@gmail.com>
2024-12-25 12:30:56 +0300

    add foreign key separately at the end

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,6 @@ USE `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `cities` (
        `id`    	  INT		NOT NULL UNIQUE AUTO_INCREMENT,
        `state_id` INT 		NOT NULL,
-       `name`     VARCHAR(256)  NOT NULL,
-       FOREIGN KEY(`state_id`)
-       REFERENCES `hbtn_0d_usa`.`state`(`id`),
-       PRIMARY KEY(`id`)
-);
+       `name` 	  VARCHAR(256)  NOT NULL,
+       FOREIGN KEY (`state_id`) REFERENCES states(id)
+   );
