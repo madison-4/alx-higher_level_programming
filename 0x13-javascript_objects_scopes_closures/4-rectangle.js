9755598e0976b6d16d60fa8c2e913ddc5574b459
noxcodes <samingosi97@gmail.com>
2025-01-02 21:12:50 +0300

    follow semistad indents

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,14 +18,16 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
-    rotate () {
-	let place = this.width;
-	this.width = this.height;
-	this.height = place;
-    }
-    double () {
-	this.width = this.width * 2;
-	this.height = this.height * 2;
-    }
+
+  rotate () {
+    const place = this.width;
+    this.width = this.height;
+    this.height = place;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
 }
 module.exports = Rectangle;
