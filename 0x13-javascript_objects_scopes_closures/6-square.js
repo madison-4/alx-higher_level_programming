33c9fd786afd8d938f87e58ebf06f65e28659fe5
noxcodes <samingosi97@gmail.com>
2025-01-03 01:54:54 +0300

    use require with 5-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,47 +1,18 @@
 #!/usr/bin/node
 // a script to create a rectangle class
-/**
- * a class that defines the width and height
- * It employs getters and setters
- */
 
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+const Squares = require('./5-square.js');
+class Square extends Squares {
+    constructor (size) {
+  super(size);
+   }
+  charPrint(c) {
+  if (c === undefined) {
+       c = 'X';
+  }
+   for (let i = 0; i < this.size; i++) {
+     console.log(c.repeat(this.size));
+	}
     }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const place = this.width;
-    this.width = this.height;
-    this.height = place;
-  }
-
-  double () {
-    this.width = this.width * 2;
-    this.height = this.height * 2;
-  }
-}
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.size; i++) {
-      console.log(c.repeat(this.size));
-    }
-  }
 }
 module.exports = Square;
