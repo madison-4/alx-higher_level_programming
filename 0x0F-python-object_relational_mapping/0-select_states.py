d66f83ee8d3961bb159156d6092131c3981a78f7
noxcodes <samingosi97@gmail.com>
2025-03-04 23:31:03 +0300

    use list comr

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,26 +3,11 @@
 # Usage: ./0-select_states.py <mysql username> \
 #                             <mysql password> \
 #                             <database name>
-"""A module to get all staes from the given database
-    The database and passwords are given as commandline arguments
-
-    # a script to list all states from a database
-
-    Attributes:
-       The first arg is the user second, the password, third the database
-"""
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    """ This module should not be executed when imported
-    """
-
-    import sys
-    import MySQLdb
-
-    db = MySQLdb.connect(host=127.0.0.1: 3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    states = cur.fetchall()
-    for state in states:
-        print(f"{state}")
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
