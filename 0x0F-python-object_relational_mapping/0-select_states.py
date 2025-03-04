19f50fed6d130f0778ca799e9faa60c3dfd3f1bd
noxcodes <samingosi97@gmail.com>
2025-03-04 20:05:36 +0300

    priont using f-strings

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 # a script to list all states from a database
 
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host=127.0.0.1: 3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    states = cur.fetchall()
+    for state in states:
+        print(f"{state}")
