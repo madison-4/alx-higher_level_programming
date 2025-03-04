9dd6f0297dd8b604ff15922be7e69d86e3d79fdb
noxcodes <samingosi97@gmail.com>
2025-03-04 20:24:18 +0300

    remove space in code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     import MySQLdb
 
     db = MySQLdb.connect(host=127.0.0.1: 3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     states = cur.fetchall()
