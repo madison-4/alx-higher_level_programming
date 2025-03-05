ed8b3286be6825fd106be8b0fa8dd8b3de7d7ba1
noxcodes <samingosi97@gmail.com>
2025-03-05 21:48:24 +0300

    use p[port keyword

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     import sys
     import MySQLdb
-    db = MySQLdb.connect(host="localhost:3306", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
