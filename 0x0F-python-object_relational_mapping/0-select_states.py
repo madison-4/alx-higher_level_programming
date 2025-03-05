7655c30531aaa99a4caf124b48241b0f3ab6aff4
noxcodes <samingosi97@gmail.com>
2025-03-05 10:06:19 +0300

    use kwargs of hosti.e. unstring teh keyword

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     import sys
     import MySQLdb
-    db = MySQLdb.connect("host=127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="127.0.0.1:3306", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
