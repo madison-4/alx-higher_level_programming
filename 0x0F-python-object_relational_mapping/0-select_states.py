c258125ac89940389a71b5442013264d3b44cace
noxcodes <samingosi97@gmail.com>
2025-03-05 09:59:31 +0300

    import after docstrings

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
 """ A script to list all states from a database
     The db, username and passwd are args
 """
@@ -9,6 +7,8 @@ if __name__ == "__main__":
     """ The code should not be executed when imported
     """
 
+    import sys
+    import MySQLdb
     db = MySQLdb.connect("host=127.0.0.1: 3036", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
