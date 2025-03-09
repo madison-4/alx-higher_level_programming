b57cd83e80401e7e6e2c13f6d69d5372ac852a8f
noxcodes <samingosi97@gmail.com>
2025-03-09 20:16:17 +0300

    use startwswith to get fiorst letter N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A script to list all states from a database
+    The db, username and passwd are args
+"""
+
+if __name__ == "__main__":
+    """ The code should not be executed when imported
+    """
+
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    states = c.fetchall()
+    for state in states:
+        if (state[1].startswith('N'):
+            print(state)
