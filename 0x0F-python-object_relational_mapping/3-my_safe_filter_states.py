e2104460cba9305f494524716623c1d69deecd2a
noxcodes <samingosi97@gmail.com>
2025-03-15 02:26:58 +0300

    call str with the modulo operator

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` WHERE BINARY name = %(state)",
-              {'state': sys.argv[4]})
+              {'state': str(sys.argv[4]}))
     states = c.fetchall()
     for state in states:
         print(state)
