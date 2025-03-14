b8fd0d3fae38bd665b295e6fca0e1d794a2f7c85
noxcodes <samingosi97@gmail.com>
2025-03-15 02:21:54 +0300

    use the modulo operator

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states` WHERE BINARY name = \
-                            '{:'s'}'".format(str(sys.argv[4])))
+    c.execute("SELECT * FROM `states` WHERE BINARY name = %(state)",\
+              {'state':sys.argv[4]})
     states = c.fetchall()
     for state in states:
         print(state)
