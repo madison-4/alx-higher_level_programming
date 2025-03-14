7877c7e2935d2f016f09bdf694d10db675b480e9
noxcodes <samingosi97@gmail.com>
2025-03-14 08:47:17 +0300

    format with string

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` WHERE BINARY name = \
-                            '{}'".format(sys.argv[4],replace("'","\'")))
+                            '{:s}'".format(sys.argv[4]))
     states = c.fetchall()
     for state in states:
         print(state)
