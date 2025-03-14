b5d917c4abac8f9dbf622e2cf1432e71f450c193
noxcodes <samingosi97@gmail.com>
2025-03-15 02:57:57 +0300

    use str on the arg

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,11 +11,13 @@ if __name__ == "__main__":
 
     import sys
     import MySQLdb
+    import mysql.connector
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
+    term = mysql.connector.connector.escape_string(sys.argv[4])
     c.execute("SELECT * FROM `states` WHERE name = {:s}"
-              .str(format(sys.argv[4])))
+              .format(str(sys.argv[4])))
     states = c.fetchall()
     for state in states:
         print(state)
