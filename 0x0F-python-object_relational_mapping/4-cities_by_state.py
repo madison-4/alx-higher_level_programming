8b0adc2800d8d4389ca98df2e2646c6c1e3c35fc
noxcodes <samingosi97@gmail.com>
2025-03-15 03:21:06 +0300

    select ordered cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A script to list all states from a database
+    The db, username and passwd are args
+    It prints states whose names are the fourth argument
+    It is safe from sql injections
+"""
+
+if __name__ == "__main__":
+    """ The code should not be executed when imported
+    """
+
+    import sys
+    import MySQLdb
+    if (len(sys.argv) != 4):
+        print("Can only take 3 arguments")
+        sys.exit(1)
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` ORDER BY cities.id ASC;")
+    cities = c.fetchall()
+    for city in cities:
+        print(city)
+    c.close()
+    db.close
