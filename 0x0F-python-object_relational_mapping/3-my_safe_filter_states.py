de06a82f354fcea570ad8e03a6c2edea33e978a9
noxcodes <samingosi97@gmail.com>
2025-03-15 03:11:41 +0300

    just use raw input

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,15 +11,13 @@ if __name__ == "__main__":
 
     import sys
     import MySQLdb
-    import mysql.connector
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    term = mysql.connector.escape_string(sys.argv[4])
     c.execute("SELECT * FROM `states`")
     states = c.fetchall()
     for state in states:
-        if state[1] == term:
+        if state[1] == sys.argv[4]:
             print(state)
     c.close()
     db.close
