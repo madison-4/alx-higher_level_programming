a7d37ea11d2e4d8eb220bf199b4122e5c7101c4b
noxcodes <samingosi97@gmail.com>
2025-03-15 03:24:07 +0300

    use backticks

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `cities` ORDER BY cities.id ASC;")
+    c.execute("SELECT * FROM `cities` ORDER BY `id` ASC")
     cities = c.fetchall()
     for city in cities:
         print(city)
