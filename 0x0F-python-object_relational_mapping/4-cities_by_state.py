0e11644af78954e4e2ce5168f1e8466482149eaa
noxcodes <samingosi97@gmail.com>
2025-03-21 13:25:10 +0300

    use backticks separately on table and column name

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`" +
               "FROM `cities`" +
-              "JOIN `states` ON `cities`.`state_id` = `states.id`" +
+              "JOIN `states` ON `cities`.`state_id` = `states`.`id`" +
               "ORDER BY `cities.id` ASC;")
     places = c.fetchall()
     for city in places:
