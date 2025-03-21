d7300053deb753c0d5c847a9b5bf21bd2996845d
noxcodes <samingosi97@gmail.com>
2025-03-21 23:34:49 +0300

    use string format

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script to list all states from a database
+    The db, username and passwd are args
+    It prints states whose names are the fourth argument
+    prints ordered cities
+"""
+
+if __name__ == "__main__":
+    """ The code should not be executed when imported
+    """
+
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
+              FROM `cities`\
+              JOIN `states` ON `cities`.`state_id` = `states`.`id`\
+              WHERE `states`.`name` = {:s}
+              ORDER BY `cities`.`id` ASC;".format(sys.argv[4])
+    places = c.fetchall()
+    for city in places:
+        print(city)
+    c.close()
+    db.close
