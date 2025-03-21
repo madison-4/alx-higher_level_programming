889d2b775d2f08c4193835fd1d82b6c4a76ab520
noxcodes <samingosi97@gmail.com>
2025-03-21 13:30:25 +0300

    see if it works with the slashes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,10 +17,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`" +
-              "FROM `cities`" +
-              "JOIN `states` ON `cities`.`state_id` = `states`.`id`" +
-              "ORDER BY `cities`.`id` ASC;")
+    c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
+              FROM `cities`\
+              JOIN `states` ON `cities`.`state_id` = `states`.`id`\
+              ORDER BY `cities`.`id` ASC;")
     places = c.fetchall()
     for city in places:
         print(city)
