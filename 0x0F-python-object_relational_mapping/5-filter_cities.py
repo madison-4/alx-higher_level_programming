547520a752fb3424c4758280239eb9c8946998ef
noxcodes <samingosi97@gmail.com>
2025-03-21 23:50:10 +0300

    no input in sql query]

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,13 +14,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT `cities`.`id`, `cities`.`name`, `states`.`name`\
+    c.execute("SELECT `cities`.`name`, `states`.`name`\
               FROM `cities`\
               JOIN `states` ON `cities`.`state_id` = `states`.`id`\
-              WHERE `states`.`name` = {:s} \
-              ORDER BY `cities`.`id` ASC;".format(sys.argv[4]))
+              ORDER BY `cities`.`id` ASC;")
     places = c.fetchall()
-    for city in places:
-        print(city)
+    print(", ".join([city[0] for city in places if city[1] == sys.argv[4]]))
     c.close()
     db.close
