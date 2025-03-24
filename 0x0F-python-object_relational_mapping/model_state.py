305a2a103fc88bb5fbef0c353507dbd9e13eb000
noxcodes <samingosi97@gmail.com>
2025-03-24 03:53:08 +0300

    initial orm commit

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -14,12 +14,4 @@ if __name__ == "__main__":
     import SQLAlchemy
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `cities`.`name`, `states`.`name`\
-              FROM `cities`\
-              JOIN `states` ON `cities`.`state_id` = `states`.`id`\
-              ORDER BY `cities`.`id` ASC;")
-    places = c.fetchall()
-    print(", ".join([city[0] for city in places if city[1] == sys.argv[4]]))
-    c.close()
     db.close
