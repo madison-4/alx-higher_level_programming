30aa828aa6b0963b994efca1d59b21283a9b13be
noxcodes <samingosi97@gmail.com>
2025-04-03 04:08:12 +0300

    make orm state file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-""" A script to list all states from a database
-    The db, username and passwd are args
-    It prints states whose names are the fourth argument
-    prints ordered cities
+""" A script to connect a class StaTE to the table sttes
+    it connects to a MySQL server locally hosted
 """
 
 if __name__ == "__main__":
@@ -11,7 +9,18 @@ if __name__ == "__main__":
 
     import sys
     import MySQLdb
-    import SQLAlchemy
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    db.close
+    from sqlalchemy import Column, Integer, String, create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+
+    Base = declarative_base()
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(sys.argv[1],
+                                                              sys.argv[2],
+                                                              sys.argv[3])
+    class State(Base):
+        """ The state class for the states table
+        """
+
+        __tablename__ = "states"
+        id = Column(Integer, primary_key=True)
+        name = Column(String(128), nullable=False)
+        
