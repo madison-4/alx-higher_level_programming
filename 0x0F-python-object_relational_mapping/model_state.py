be5424f62b7eeb921f222d558fdf324f9ab82b79
noxcodes <samingosi97@gmail.com>
2025-04-18 16:56:14 +0300

    create a states table

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,9 +4,27 @@
     uses port 3306
 """
 
-from sqlalchemy import create_engine
 import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
-db_url = f"mysql+mysqldb://{sys.argv[1]}:{sys.qrv[2]}@localhost:3306/{sys.argv[3]}"
-engine = create_engine(db_url,echo=True)
 
+db_url = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}"+
+f"@localhost:3306/{sys.argv[3]}"
+engine = create_engine(db_url, echo=True)
+Base = declarative_base()
+
+
+class State(Base):
+    """ A class that maps to the states table
+        This class will define the table
+    """
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
+Base.metadata.create_all(engine)
