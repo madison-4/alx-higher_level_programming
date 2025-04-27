6ef21abb5dd8bd9d52174d1cf9348595ef97bf6b
noxcodes <samingosi97@gmail.com>
2025-04-28 02:19:41 +0300

    gc

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""A script that lists all objects from a databse given to it
-It uses a mysql database and connects to it
-"""
 
+
+"""script that lists all State objects from the database hbtn_0e_6_usa."""
 
 import sys
-from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
 
 
-def printword():
-    """ This function does the printing
-    """
-    use, word, dbase = sys.argv[1:]
-    dburl = f"mysql+mysqldb://{use}:{word}@localhost:3306/{dbase}"
-    engine = create_engine(dburl, pool_pre_ping=True)
+def list_class(u_name, p_word, db_name):
+    query_1 = f'mysql://{u_name}:{p_word}@localhost:3306/{db_name}'
+    engine = create_engine(query_1, pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
+
     states = session.query(State).order_by(State.id.asc()).all()
     for state in states:
         print(f"{state.id}: {state.name}")
+
     session.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     if len(sys.argv) != 4:
-        print("Error, too few arguments")
+        print("Erro")
         sys.exit(1)
-    printword()
+
+    u_name, p_word, db_name = sys.argv[1:]
+    list_class(u_name, p_word, db_name)
