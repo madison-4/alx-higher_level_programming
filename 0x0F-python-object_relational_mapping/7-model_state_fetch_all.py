59f5ddda42325fbadf1927d753a0259f1a3453ea
noxcodes <samingosi97@gmail.com>
2025-04-28 06:52:48 +0300

    use a function to do all the work

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
+""" A module to print states in ascending order by their state id
+It prints states from a db gicven to it
+"""
 
-
-"""script that lists all State objects from the database hbtn_0e_6_usa."""
 
 import sys
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from model_state import Base, State
 
 
-def list_class(u_name, p_word, db_name):
-    query_1 = f'mysql://{u_name}:{p_word}@localhost:3306/{db_name}'
-    engine = create_engine(query_1, pool_pre_ping=True)
+def statesprint(username, password, database):
+    """ This function connects to the database asks for the state record
+    the in prints them.
+    """
+
+
+    dburl = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    engine = create_engine(dburl, pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = session.query(State).order_by(State.id.asc()).all()
+
     for state in states:
         print(f"{state.id}: {state.name}")
+    session.close
 
-    session.close()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Erro")
+        print("Incorrect number pof arguments")
         sys.exit(1)
-
-    u_name, p_word, db_name = sys.argv[1:]
-    list_class(u_name, p_word, db_name)
+    username, password, database = sys.argv[1:4]
+    statesprint(username, password, database)
