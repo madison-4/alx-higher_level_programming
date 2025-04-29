e9ca09911815ae76d60716d631c3c5e28b8c9742
noxcodes <samingosi97@gmail.com>
2025-04-29 18:12:46 +0300

    porint the new state id

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" A module to add the louisiana state to the db
+"""
+
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+
+def statesprint(username, password, database, state_name):
+    """ This function adds the Louisian state
+    the in prints them.
+    """
+
+    dburl = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
+    engine = create_engine(dburl, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name = 'Louisiana')
+    session.add(state)
+    session.commit()
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
+    print(f'{new_state.id}')
+    session.close
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Incorrect number of arguments")
+        sys.exit(1)
+    username, password, database, state_name = sys.argv[1:4]
+    statesprint(username, password, database)
