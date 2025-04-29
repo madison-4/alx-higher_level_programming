a8e6e69e7e8b26ad71fc1aba90b4eba9e7f4dde2
noxcodes <samingosi97@gmail.com>
2025-04-29 16:59:01 +0300

    use ilike method

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" A module to print states in ascending order by their state id
-It prints states with the letter a from a db gicven to it
+""" A module to print states in ascending order by the state
+ whose name is given as the fourth argument
+It is also free from sql injections
 """
 
 
@@ -10,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 
-def statesprint(username, password, database):
+def statesprint(username, password, database, state_name):
     """ This function connects to the database asks for the state record
     the in prints them.
     """
@@ -21,13 +22,17 @@ def statesprint(username, password, database):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(User.name.ilike('a')).order_by(State.id.asc()).all()
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        if 'a' in state.name:
+            print(f"{state.id}: {state.name}")
     session.close
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print("Incorrect number pof arguments")
         sys.exit(1)
-    username, password, database = sys.argv[1:4]
-    statesprint(username, password, database)
+    username, password, database, state_name = sys.argv[1:5]
+    statesprint(username, password, database, state_name)
