08215c7301195e2ff1a3739513275f061d1cd20f
noxcodes <samingosi97@gmail.com>
2025-04-29 17:07:44 +0300

    use % signs with ilike arg

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -21,7 +21,8 @@ def statesprint(username, password, database):
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).filter(User.name.ilike('a')).order_by(State.id.asc()).all()
+    states = session.query(State).filter(User.name.ilike('%a%'))
+    .order_by(State.id.asc()).all()
     session.close
 
 
