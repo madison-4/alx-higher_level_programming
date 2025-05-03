828c58fa2edc32ea2a36fcde807e01cfc7bcf9c6
noxcodes <samingosi97@gmail.com>
2025-05-04 02:12:11 +0300

    djb

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
-""" A module to print cities in ascending order by their city id
-It prints cities from a db gicven to it
-"""
-
-
+# Lists all City objects from the database hbtn_0e_14_usa.
+# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
+#                                          <mysql password> /
+#                                          <database name>
 import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
-
-def statesprint(username, password, database):
-    """ This function connects to the database asks for the city record
-    then in prints them.
-    """
-
-    dburl = f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}"
-    engine = create_engine(dburl, pool_pre_ping=True)
-
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-
-    for city, state in session.query(City, State)\
-            .filter(City.state_id == State.id)\
-            .order_by(City.id):
-        print(f"{state.name}: ({city.id}) {city.name}")
-    session.close
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Incorrect number pof arguments")
-        sys.exit(1)
-    username, password, database = sys.argv[1:4]
-    statesprint(username, password, database)
+    for city, state in session.query(City, State) \
+                              .filter(City.state_id == State.id) \
+                              .order_by(City.id):
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
